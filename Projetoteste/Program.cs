580e97d2e22652d4e7640da04d1dcc3f84e7d18c
Eduardo Cercal <eduardo_cercal@hotmail.com>
2021-07-14 19:45:31 -0300

    Projeto iniciado, mostrando a mensagem hello word

---
+++ Projetoteste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projetoteste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
